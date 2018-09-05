79494edf47dba030484f10a682d62ada0a1e24a5
Margreet Dijkstra <dijkstra.margreet@gmail.com>
2018-09-05 20:50:42 +0100

    -

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,11 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @review = Review.all
+  end
+
+  def new
+    @review = Review.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # get "reviews" to: "reviews#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'reviews#index'
+
+  get 'reviews/new', to: 'reviews#new'
+    resources :reviews
 end
